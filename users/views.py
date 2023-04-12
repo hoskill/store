335dd0d382e39fa1983a6e37779d0d5610cb47bc
hoskill <kravets42@mail.ru>
2023-04-12 16:07:38 +0700

    Логин и логаут переписаны на классы. Создан миксин для дублирующейся информации в контексте. Исправлен базовый шаблон в приложении "users"

--- users/views.py
+++ users/views.py
@@ -3,39 +3,45 @@ from django.contrib import auth, messages
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
+from common.views import TitleMixin
 
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-
-    context = {'form': form}
-    return render(request, 'users/login.html', context)
+class UserLoginView(TitleMixin, LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+    title = 'Store - Авторизация'
 
 
-class UserRegistrationView(CreateView):
+# def login(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username=username, password=password)
+#             if user:
+#                 auth.login(request, user)
+#                 return HttpResponseRedirect(reverse('index'))
+#     else:
+#         form = UserLoginForm()
+#
+#     context = {'form': form}
+#     return render(request, 'users/login.html', context)
+
+
+class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
     model = User
     form_class = UserRegistrationForm
     template_name = 'users/registration.html'
     success_url = reverse_lazy('users:login')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data()
-        context['title'] = 'Store - Регистрация'
-        return context
+    success_message = 'Регистрация прошла успешно!'
+    title = 'Store - Регистрация'
 
 
 # def registration(request):
@@ -52,20 +58,19 @@ class UserRegistrationView(CreateView):
 #     return render(request, 'users/registration.html', context)
 
 
-class UserProfileView(UpdateView):
+class UserProfileView(TitleMixin, UpdateView):
     model = User
     form_class = UserProfileForm
     template_name = 'users/profile.html'
+    title = 'Store - Профиль'
 
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['title'] = 'Store - Профиль'
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
-
 
 # @login_required
 # def profile(request):
@@ -88,6 +93,7 @@ class UserProfileView(UpdateView):
 #     return render(request, 'users/profile.html', context)
 
 
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
+# Заменен на встроенный класс LogoutView сразу в URLs, т.к. не изменен базовый функционал.
+# def logout(request):
+#     auth.logout(request)
+#     return HttpResponseRedirect(reverse('index'))
